5ed2266a7736a375deb2d120e0a2fa58023a4345
xinlin <xinlin.zhang@hotmail.com>
2020-11-07 22:15:51 +0100

    add cost to plot

--- 001_gd_mnist_fmnist.py
+++ 001_gd_mnist_fmnist.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import nnet
 import dataset as ds
@@ -18,32 +19,47 @@ nn_size = (784,120,10)
 
 # two same network for mnist and fmnsit
 mnn = nnet.fffnn(nn_size)
-mnn.load(activation_func =    func.sigmoid,
-         activation_func_dz = func.sigmoid_dz,
-         cost_func =          func.cross_entropy_x,
-         cost_func_da =       func.cross_entropy_x_da)
+mnn.load(
+    activation_func =    func.sigmoid,
+    activation_func_dz = func.sigmoid_dz,
+    cost_func =          func.cross_entropy_x,
+    cost_func_da =       func.cross_entropy_x_da
+)
+
 fnn = nnet.fffnn(nn_size)
-fnn.load(activation_func =    func.sigmoid,
-         activation_func_dz = func.sigmoid_dz,
-         cost_func =          func.cross_entropy_x,
-         cost_func_da =       func.cross_entropy_x_da)
+fnn.load(
+    activation_func =    func.sigmoid,
+    activation_func_dz = func.sigmoid_dz,
+    cost_func =          func.cross_entropy_x,
+    cost_func_da =       func.cross_entropy_x_da
+)
 
 # plot
 plt.ion()
-fig = plt.figure('teapot 001')
+fig = plt.figure('Teapot : %s' % sys.argv[0])
 fig.suptitle('MNIST vs. FMNIST')
-ax = fig.add_subplot()
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
 
 # rocking
 epochs = []
 amlist = []
 aflist = []
-ax.set_title('NN:%s, Learning Rate:%d, Epoch:%d'%(str(nn_size),eta,epoch))
-ax.plot(epochs, amlist, linewidth=0.5, color='r', label='MNIST')
-ax.plot(epochs, aflist, linewidth=0.5, color='b', label='FMNIST')
-ax.legend()
-ax.set_xlabel('epoch')
-ax.set_ylabel('accuracy')
+mcost = []
+fcost = []
+
+ax1.set_title('NN:%s, Learning Rate:%d, Epoch:%d'%(str(nn_size),eta,epoch))
+ax1.plot(epochs, amlist, linewidth=0.5, color='r', label='MNIST')
+ax1.plot(epochs, aflist, linewidth=0.5, color='b', label='FMNIST')
+ax1.legend()
+ax1.set_xlabel('epoch')
+ax1.set_ylabel('accuracy')
+
+ax2.plot(epochs, mcost, linewidth=0.5, color='r', label='MNIST')
+ax2.plot(epochs, fcost, linewidth=0.5, color='b', label='FMNIST')
+ax2.legend()
+ax2.set_xlabel('epoch')
+ax2.set_ylabel('cost')
 
 for i in range(1,epoch):
     epochs.append(i)
@@ -56,6 +72,7 @@ for i in range(1,epoch):
     accuracy = sum([int(x==y) for x,y in zip(test_result,right_result)])
     amlist.append(accuracy)
     cost = mnn.cost(mtedy, mtedx)
+    mcost.append(cost)
     print(' mnist update, accuracy: %d, cost: %f' % (accuracy, cost))
     # fmnist
     fnn.gd(ftrdx, ftrdy, eta)
@@ -65,17 +82,20 @@ for i in range(1,epoch):
     accuracy = sum([int(x==y) for x,y in zip(test_result,right_result)])
     aflist.append(accuracy)
     cost = fnn.cost(ftedy, ftedx)
-    print('fmnist update, accuracy: %d, cost: %f' % ( accuracy, cost))
+    fcost.append(cost)
+    print('fmnist update, accuracy: %d, cost: %f' % (accuracy, cost))
     # plot
     try:
-        ax.plot(epochs, amlist, linewidth=0.5, color='r', label='MNIST')
-        ax.plot(epochs, aflist, linewidth=0.5, color='b', label='FMNIST')
+        ax1.plot(epochs, amlist, linewidth=0.5, color='r', label='MNIST')
+        ax1.plot(epochs, aflist, linewidth=0.5, color='b', label='FMNIST')
+        ax2.plot(epochs, mcost,  linewidth=0.5, color='r', label='MNIST')
+        ax2.plot(epochs, fcost,  linewidth=0.5, color='b', label='FMNIST')
         plt.pause(0.001)
     except:
         ...
 
 
-print('end')
+print('Done!')
 plt.ioff()
 plt.show()
 
