a49fb04f01296ed0349abdca5622612a2b5ce0aa
xinlin <xinlin.zhang@hotmail.com>
2020-11-10 14:33:34 +0100

    add knn.py

---
+++ 0030_knn_mnist_fmnist.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import dataset as ds
+from knn import knn
+
+
+# dataset load
+mtrdx, mtrdy, mtedx, mtedy = ds.load_mnist()
+ftrdx, ftrdy, ftedx, ftedy = ds.load_fmnist()
+
+K = 10
+xlabels = ('MNIST','FMNIST')
+x = np.arange(len(xlabels))
+width = 0.3
+
+fig = plt.figure('Teapot: KNN on MNIST & FMNIST')
+fig.suptitle('KNN on MNIST & FMNIST')
+ax = fig.add_subplot()
+perf_s = [0,0]
+perf_f = [0,0]
+rects1 = ax.bar(x-width/2, perf_s, 0.4, color='g', alpha=0.7, label='Success')
+rects2 = ax.bar(x+width/2, perf_f, 0.4, color='r', alpha=0.7, label='Fail')
+ax.set_ylabel('Scores')
+ax.set_xticks(x)
+ax.set_xticklabels(xlabels)
+ax.legend()
+ax.set_title('K = %d' % K)
+fig.tight_layout()
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    anno = []
+    for rect in rects:
+        height = rect.get_height()
+        a = ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 1),  # 1 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+        anno.append(a)
+    return anno
+
+
+anno1 = autolabel(rects1)
+anno2 = autolabel(rects2)
+plt.ion()
+
+for i in range(10000):
+    p = knn(mtrdx, mtrdy, mtedx[:,i].reshape(784,1), K)
+    t = np.argmax(mtedy[:,i])
+    if p == t:
+        perf_s[0] += 1
+    else:
+        perf_f[0] += 1
+    print(' mnist, prediction=%d, truth=%d, %s'
+                    %(p,t,'Success' if p==t else 'Fail'))
+
+    p = knn(ftrdx, ftrdy, ftedx[:,i].reshape(784,1), K)
+    t = np.argmax(ftedy[:,i])
+    if p == t:
+        perf_s[1] += 1
+    else:
+        perf_f[1] += 1
+    print('fmnist, prediction=%d, truth=%d, %s'
+                    %(p,t,'Success' if p==t else 'Fail'))
+
+    [(x.remove(),y.remove()) for x,y in zip(rects1,rects2)]
+    rects1 = ax.bar(x-width/2, perf_s, 0.4, color='g', alpha=0.7)
+    rects2 = ax.bar(x+width/2, perf_f, 0.4, color='r', alpha=0.7)
+    [(x.remove(),y.remove()) for x,y in zip(anno1,anno2)]
+    anno1 = autolabel(rects1)
+    anno2 = autolabel(rects2)
+
+    plt.pause(0.001)
+
+
+plt.ioff()
+plt.show()
+
+
---
+++ knn.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def knn(x, y, t, K):
+    """Use the same data format with dataset.load_mnist api."""
+    dist = np.linalg.norm(x-t, axis=0)
+    col = np.argsort(dist)
+    kset = [(dist[col[i]],np.argmax(y[:,col[i]])) for i in range(K)]
+    near = [0 for i in range(len(y.T))]
+    for it in kset:
+        near[it[1]] += 1
+    tm = max(near)  # the max
+    if near.count(tm) == 1:
+        return near.index(tm)
+    else:
+        # choose the min dist one
+        mdist = []  # dist of those maxes
+        for i in range(near.count(tm)):
+            itm = near.index(tm)
+            tdist = 0
+            for it in kset:
+                if it[1] == itm:
+                    tdist += it[0]
+            mdist.append((tdist,itm))
+            near[itm] = 0  # change near list
+        mdist.sort(key=lambda x:x[0])
+        return mdist[0][1]
+
